2d9bdcad99c7f42680a821f5edba8394b42c799a
Otar Vega <vecor.tarfer22@gmail.com>
2026-04-15 10:31:38 -0500

    Commit inicial

---
+++ src/main/java/com/mercado/pagos/servicios/PagosServiciosMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.pagos.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosServiciosMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosServiciosMercadoApplication.class, args);
+	}
+
+}
